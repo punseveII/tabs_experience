c2a7b35e47798a97a98a52a58e3b8484255d78ee
Ella Venice Punay <ella.punay@usjr.edu.ph>
2023-01-02 21:44:55 +0800

    Changes

--- tabs_experience/src/index.js
+++ tabs_experience/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './tabs.css';
 import Tabs from './Tabs';
 import reportWebVitals from './reportWebVitals';
 
